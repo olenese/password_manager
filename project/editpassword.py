4de113b48dc4be9e65b8a88535728f625a7fb914
Ole Nese <ole.s.nese@hotmail.com>
2022-05-23 13:29:27 +0000

    Got somewhere with the editlogin

--- project/editpassword.py
+++ project/editpassword.py
@@ -2,7 +2,7 @@ from collections import UserDict
 from distutils.dep_util import newer_pairwise
 from email.generator import Generator
 from flask import Blueprint, render_template, request
-from flask_login import login_required, current_user
+from flask_login import login_required, current_user, user_logged_in
 from .models import Logins, Users
 from .security import encrypt, decrypt
 from .generatorbackend import randomcharacter
@@ -18,10 +18,40 @@ editpassword = Blueprint('editpassword', __name__)
 @editpassword.route('/editpassword')
 @login_required
 def changepassword():
-    return render_template('editpassword.html')
+    userlogins = Logins.query.filter_by(userID=current_user.id).all()
+    print(userlogins)
+    # Creates a list to store the logins from the database.
+    new_login_list = []
+    for i in userlogins:
+        # Adds the logins to a new list with the decrypted password to make the user able to see their password.
+        decrypted_login = Logins(id=i.id, location=i.location, username=i.username, password=decrypt(i.password), url=i.url, note=i.note, userID=current_user.id)
+        print(decrypted_login)
+        # Appends the new list to the new_login_list list for each login. 
+        new_login_list.append(decrypted_login)
+    print (new_login_list.location)
+
+    return render_template('editpassword.html', website=new_login_list)
 
 @editpassword.route('/editpassword', methods=['POST'])
 @login_required
 def changepassword_post():
-    return render_template('editpassword.html')
 
+    editpasswordlocation = request.form.get('location')
+    newpassword = request.form.get('newpassword')
+
+
+    postnewpassword = Users(userID=current_user.id, location=editpasswordlocation, password=encrypt(newpassword))
+    db.session.update(postnewpassword)
+    db.session.commit()
+
+    return render_template('editpassword.html' )
+
+    userlogins = Logins.query.filter_by(userID=current_user.id).all()
+    # Creates a list to store the logins from the database.
+    new_login_list = []
+    for i in userlogins:
+        # Adds the logins to a new list with the decrypted password to make the user able to see their password.
+        decrypted_login = Logins(id=i.id, location=i.location, username=i.username, password=decrypt(i.password), url=i.url, note=i.note, userID=current_user.id)
+        # Appends the new list to the new_login_list list for each login. 
+        new_login_list.append(decrypted_login)
+    # Sends the new_login_list list to the viewlogins.html page to be displayed.
