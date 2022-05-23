4e7bed45b616178bb58b91646874127f1269eae0
Ole Nese <ole.s.nese@hotmail.com>
2022-05-23 11:27:36 +0000

    fixed main.py

--- project/main.py
+++ project/main.py
@@ -12,7 +12,6 @@ main = Blueprint('main', __name__)
 # Creates the landing page for new users and what shows when they log in
 @main.route('/')
 def index():
-    checkpwn()
     return render_template('index.html')
 
 # Creates the profile page for users, the users are required to be logged in to view this page
